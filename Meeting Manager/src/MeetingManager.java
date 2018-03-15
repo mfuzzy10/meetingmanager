bcd47a421fba2f2716714102859cdece6face92a
ArchieRutherford <ArchieRutherford@computing.dundee.ac.uk>
2018-03-15 12:02:09 +0000

    Started.
    Just added an empty main and some comments.

---
+++ Meeting Manager/src/MeetingManager.java
@@ -0,0 +1,21 @@
+/**
+ * This program is a Meeting Manager for an office so 
+ * employees can store and keep track of all of the 
+ * meetings that they have in the foreseeable future.
+ * 
+ * @author Archie Rutherford
+ * @version 1.0
+ */
+
+public class MeetingManager {
+
+	/**
+	 * Main Method of the program.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+}
