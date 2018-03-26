707746c1ef2f570fc7714e12b7ad1d7b27a20c79
Oskar Aleksander Jankowski <oskar.jankowski.oj@gmail.com>
2018-03-26 02:20:38 +0100

    Add files via upload

---
+++ Meeting Manager/src/TimePeriod.java
@@ -0,0 +1,46 @@
+import java.time.LocalDateTime;
+
+public class TimePeriod implements Comparable<TimePeriod> {
+
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
+	
+	public TimePeriod(LocalDateTime startTime, LocalDateTime endTime) {
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
+	
+	public int compareTo(TimePeriod compareTimePeriod) {
+
+		LocalDateTime compareStartTime = ((TimePeriod) compareTimePeriod).getStartTime();
+		
+		return this.startTime.compareTo(compareStartTime);
+		
+	}
+	
+	/**
+	 * @return the startTime
+	 */
+	public LocalDateTime getStartTime() {
+		return startTime;
+	}
+	/**
+	 * @param startTime the startTime to set
+	 */
+	public void setStartTime(LocalDateTime startTime) {
+		this.startTime = startTime;
+	}
+	/**
+	 * @return the endTime
+	 */
+	public LocalDateTime getEndTime() {
+		return endTime;
+	}
+	/**
+	 * @param endTime the endTime to set
+	 */
+	public void setEndTime(LocalDateTime endTime) {
+		this.endTime = endTime;
+	}
+	
+}
